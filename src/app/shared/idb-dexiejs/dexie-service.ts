281934decf937ae33d974ef219926ab917fe9dd6
Gerson Jean Pierre <jalukone@outlook.com>
2025-09-10 11:31:17 -0500

    dexiejs service

---
+++ src/app/shared/idb-dexiejs/dexie-service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Customer } from '@core/models/customer.model';
+import { Designer } from '@core/models/designer.model';
+import { Gallery, Stand, ICashier, CashierCut, Sale } from '@core/models/index';
+import Dexie, { EntityTable } from 'dexie';
+import { galleriesSeed } from './seeds/galleries.seed';
+import { standsSeed } from './seeds/stands.seed';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DexieService extends Dexie {
+  galleries!: EntityTable<Gallery, 'id'>;
+  stands!: EntityTable<Stand, 'id'>;
+  cashier!: EntityTable<ICashier, 'id'>;
+  sales!: EntityTable<Sale, 'id'>;
+  cashierCuts!: EntityTable<CashierCut, 'id'>;
+  designers!: EntityTable<Designer, 'id'>;
+  customers!: EntityTable<Customer, 'id'>;
+
+  constructor() {
+    super('CajaLaserVelozDB');
+    this.version(1).stores({
+      galleries: 'id, name',
+      stands: 'id, internalId, name, galleryId',
+      sales: 'id, correlative, cashierId, designerId, customerId, status, paymentMethod',
+      cashier: 'id, fullName',
+      cashierCuts: 'id, cashierId, openingTime, closingTime, status',
+      designers: 'id, fullName, isActive',
+      customers: 'id, fullName, dni, ruc, phone, email, isActive',
+    });
+
+    this.on('ready', () => this.seedData());
+  }
+
+  private async seedData() {
+    const galleryCount = await this.galleries.count();
+    if (galleryCount === 0) {
+      await this.galleries.bulkAdd(galleriesSeed);
+      await this.stands.bulkAdd(standsSeed)
+    }
+
+  }
+}
---
+++ src/app/shared/idb-dexiejs/seeds/galleries.seed.ts
@@ -0,0 +1,8 @@
+export const galleriesSeed = [
+  {
+    "id": "0198aead-a5f0-744c-8cfb-2fc86c1537e6",
+    "name": "CC Guizado Record Plaza",
+    "address": "Jr. Huaraz 1717 (Altura Cdra. 9 Av. Brasil)",
+    "isActive": true,
+  }
+]
