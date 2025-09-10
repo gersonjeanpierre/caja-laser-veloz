2b688c73f1b468474cd96949b62c0784cdf5bf9d
Gerson Jean Pierre <jalukone@outlook.com>
2025-09-10 11:33:59 -0500

    development dashboard component

---
+++ src/app/features/dashboard/dashboard.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard implements OnInit {
+
+
+  ngOnInit() {
+    // Lógica de inicialización del componente
+  }
+
+}
