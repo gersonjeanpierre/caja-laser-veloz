21f087c56b88271bf3e6b6c1d75d4c294a8b1b0e
Gerson Jean Pierre <jalukone@outlook.com>
2025-09-10 11:32:15 -0500

    add reusables components

---
+++ src/app/shared/components/crud-form/crud-form.ts
@@ -0,0 +1,44 @@
+import { Input, Output, EventEmitter, Component } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IftaLabelModule } from "primeng/iftalabel";
+import { ButtonModule } from "primeng/button";
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SelectModule } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+
+@Component({
+  selector: 'crud-form',
+  templateUrl: './crud-form.html',
+  styleUrl: './crud-form.css',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    IftaLabelModule,
+    ButtonModule,
+    InputTextModule,
+    SelectButtonModule,
+    SelectModule,
+    DatePickerModule,
+  ]
+})
+export class CrudForm {
+  @Input() form!: FormGroup;
+  @Input() fields: {
+    name: string;
+    label: string;
+    type?: string;
+    class?: string;
+    options?: { label: string; value: any; }[];
+    optionsSelect?: {
+      label: string,
+      value?: string,
+      id?: string
+      items: {
+        label: string, value: any
+      }[]
+    }[];
+  }[] = [];
+  @Output() save = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+}
