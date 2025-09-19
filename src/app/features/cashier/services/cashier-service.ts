4660ad04e1e246ff8afffb62ba70a902f6a09999
Gerson Jean Pierre <jalukone@outlook.com>
2025-09-19 18:21:32 -0500

    add payment entity

--- src/app/features/cashier/services/cashier-service.ts
+++ src/app/features/cashier/services/cashier-service.ts
@@ -2,13 +2,15 @@ import { inject, Injectable } from '@angular/core';
 import { DexieService } from '@shared/idb-dexiejs/dexie-service';
 import { ICashier } from '@core/models/cashier.model';
 import { v7 as uuidv7 } from 'uuid';
+import { CashierCut } from '@core/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CashierService {
   private cashier = inject(DexieService).cashier;
-  private dexieService = inject(DexieService);
+  private cashierCuts = inject(DexieService).cashierCuts;
+  private payments = inject(DexieService).payments;
 
   createCashier(cashier: ICashier) {
     cashier.id = uuidv7();
@@ -19,6 +21,11 @@ export class CashierService {
     return this.cashier.toArray();
   }
 
+  async openCashier(cashierId: string, initialAmount: number) {
+    const id = uuidv7();
 
+
+
+  }
 
 }
